40d13080739a4707f818be69bd58da533133b332
CHIPATISWATHIREDDY <113338947+CSwathi1518@users.noreply.github.com>
2023-10-04 17:27:00 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,14 @@
+import jav.util.Scanner;
+public class multiplicationtable{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("enter the number:");
+        int n=input.nextInt();
+        Scanner range=new Scanner(System.in);
+        System.out.print("enter the range :");
+        int r=range.nextInt();
+        for(int i=1;i<=r;i++){
+            System.out.prinln(n+"*"+i+"="+n*i);
+        }
+    }
+}
