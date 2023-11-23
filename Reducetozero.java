d9776583c1690edb64e2bd43365aaa6c97402360
CHIPATISWATHIREDDY <113338947+CSwathi1518@users.noreply.github.com>
2023-11-23 12:17:48 +0530

    Create Reducetozero.java

---
+++ Reducetozero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ReduceToZero {
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        int steps = numberOfSteps(num);
+        System.out.println("Number of steps to reduce to zero: " + steps);
+    }
+}
